c0b55305a476440d08fc1564d59ca3a87cb5f884
Favour Afolayan <favourafolayan@gmail.com>
2020-05-12 11:55:58 +0100

    add more eslint rules (#1)

--- index.js
+++ index.js
@@ -1,25 +1,101 @@
 module.exports = {
-    root: true,
-    env: {
-        node: true,
-    },
-    extends: [
-        'plugin:vue/essential',
-        'eslint:recommended',
-        '@vue/typescript/recommended',
-        '@vue/prettier',
-        '@vue/prettier/@typescript-eslint',
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "lines-between-class-members": [
+      "error",
+      "always",
+      { exceptAfterSingleLine: true },
     ],
-    parserOptions: {
-        ecmaVersion: 2020,
-    },
-    rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
-        'padding-line-between-statements': ['error', { blankLine: 'always', prev: '*', next: 'block' }],
-        'padding-line-between-statements': ['error', { blankLine: 'always', prev: '*', next: 'block-like' }],
-        'padding-line-between-statements': ['error', { blankLine: 'always', prev: '*', next: 'return' }],
-        'padding-line-between-statements': ['error', { blankLine: 'always', prev: '*', next: 'export' }],
-    }
-}
+    "padding-line-between-statements": [
+      "error",
+      {
+        blankLine: "always",
+        prev: "*",
+        next: "block",
+      },
+    ],
+    "padding-line-between-statements": [
+      "error",
+      {
+        blankLine: "always",
+        prev: "*",
+        next: "block-like",
+      },
+    ],
+    "padding-line-between-statements": [
+      "error",
+      {
+        blankLine: "always",
+        prev: "*",
+        next: "return",
+      },
+    ],
+    "padding-line-between-statements": [
+      "error",
+      {
+        blankLine: "always",
+        prev: "return",
+        next: "*",
+      },
+    ],
+    "padding-line-between-statements": [
+      "error",
+      {
+        blankLine: "always",
+        prev: "*",
+        next: "export",
+      },
+    ],
+    "padding-line-between-statements": [
+      "error",
+      {
+        blankLine: "never",
+        prev: "export",
+        next: "export",
+      },
+    ],
+    "padding-line-between-statements": [
+      "error",
+      {
+        blankLine: "always",
+        prev: "*",
+        next: "function",
+      },
+    ],
+    "padding-line-between-statements": [
+      "error",
+      {
+        blankLine: "always",
+        prev: "function",
+        next: "*",
+      },
+    ],
+    "padding-line-between-statements": [
+      "error",
+      {
+        blankLine: "always",
+        prev: "if",
+        next: "*",
+      },
+    ],
+    "padding-line-between-statements": [
+      "error",
+
+      {
+        blankLine: "always",
+        prev: ["const", "let", "var"],
+        next: "*",
+      },
+    ],
+    "padding-line-between-statements": [
+      "error",
+
+      {
+        blankLine: "any",
+        prev: ["const", "let", "var"],
+        next: ["const", "let", "var"],
+      },
+    ],
+  },
+};
